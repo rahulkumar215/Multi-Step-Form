7c17353c67b0189aa065e5a10d1ca98b6d8ed770
rahulkumar215 <rk1617015@gmail.com>
2024-08-31 23:42:30 +0530

    sidebar changed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,30 +9,10 @@ function Sidebar() {
 
   return (
     <div className={styles.sidebarContainer}>
-      <Step
-        number={1}
-        onClick={disableNavLink}
-        path={"/personal-info"}
-        heading="YOUR INFO"
-      />
-      <Step
-        number={2}
-        onClick={disableNavLink}
-        path={"/subscription-plans"}
-        heading="SELECT PLAN"
-      />
-      <Step
-        number={3}
-        onClick={disableNavLink}
-        path={"/add-ons"}
-        heading="ADD-ONS"
-      />
-      <Step
-        number={4}
-        onClick={disableNavLink}
-        path={"/finish-up"}
-        heading="SUMMARY"
-      />
+      <Step number={1} path={"/personal-info"} heading="YOUR INFO" />
+      <Step number={2} path={"/subscription-plans"} heading="SELECT PLAN" />
+      <Step number={3} path={"/add-ons"} heading="ADD-ONS" />
+      <Step number={4} path={"/finish-up"} heading="SUMMARY" />
     </div>
   );
 }
